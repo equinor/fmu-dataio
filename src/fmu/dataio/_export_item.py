f2b53a11c32375e71edfe2d88c165dc10d1be2bc
Per Olav Eide Svendsen <peesv@equinor.com>
2021-07-20 09:38:21 +0200

    black

--- src/fmu/dataio/_export_item.py
+++ src/fmu/dataio/_export_item.py
@@ -43,7 +43,7 @@ ALLOWED_CONTENTS = {
     "field_outline": {"contact": str},
     "fault_lines": None,
     "volumes": None,
-    "volumetrics": None
+    "volumetrics": None,  # or?
 }
 
 # this setting will set if subkeys is required or not. If not found in list then
