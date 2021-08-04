916b63c0bef8227c2c2d24c7b15e021b71bb854d
Jan C. Rivenæs <jan@rivenaes.net>
2021-08-04 13:34:43 +0200

    Add index to table export, cf #71

--- src/fmu/dataio/_export_item.py
+++ src/fmu/dataio/_export_item.py
@@ -64,11 +64,12 @@ class ValidationError(ValueError):
 class _ExportItem:  # pylint disable=too-few-public-methods
     """Export of the actual data item with metadata."""
 
-    def __init__(self, dataio, obj, subfolder=None, verbosity="warning"):
+    def __init__(self, dataio, obj, subfolder=None, verbosity="warning", index=False):
         self.dataio = dataio
         self.obj = obj
         self.subfolder = subfolder
         self.verbosity = verbosity
+        self.index_df = index
         self.subtype = None
         self.classname = "unset"
         self.name = "unknown"
@@ -800,7 +801,7 @@ class _ExportItem:  # pylint disable=too-few-public-methods
 
         logger.info("Exported file is %s", outfile)
         if "csv" in dataio.table_fformat:
-            obj.to_csv(outfile)
+            obj.to_csv(outfile, index=self.index_df)
             md5sum = _utils.md5sum(outfile)
             self.dataio._meta_data["format"] = "csv"
 
