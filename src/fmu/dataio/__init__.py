fc072a13f52ca9edf25a8f03e988f11337d67ca3
Per Olav Eide Svendsen <peesv@equinor.com>
2022-06-10 14:27:02 +0200

    linting

--- src/fmu/dataio/__init__.py
+++ src/fmu/dataio/__init__.py
@@ -5,7 +5,7 @@ from fmu.dataio.dataio import AggregatedData  # noqa  # type: ignore
 from fmu.dataio.dataio import ExportData  # noqa  # type: ignore
 from fmu.dataio.dataio import InitializeCase  # noqa  # type: ignore
 
-from fmu.dataio.dataio import read_metadata
+from fmu.dataio.dataio import read_metadata  # noqa
 
 try:
     from .version import version
