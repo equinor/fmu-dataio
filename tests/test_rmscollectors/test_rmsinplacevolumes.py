7f38742c12d0b42e1010c569a7665e619c2a0269
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-11-24 09:55:15 +0100

    Add tests for rmsinplacevolumes.py

---
+++ tests/test_rmscollectors/test_rmsinplacevolumes.py
@@ -0,0 +1,31 @@
+"""Test fuc"""
+import pytest
+import roxar
+from fmu.dataio.rmscollectors.volumetrics import RmsInplaceVolumes
+
+DROGON_PATH = "/project/fmu/tutorial/drogon/resmod/ff/users/dbs/23.1.1/"
+
+
+@pytest.fixture(name="drogon_project", scope="session")
+def _fix_drogon_project():
+    """Return drogon rms project
+
+    Returns:
+        roxar.Project: instance of drogon project
+    """
+    drogon_rms_path = f"{DROGON_PATH}rms/model/drogon.rms13.1.2/"
+
+    proj = roxar.Project.open(drogon_rms_path, readonly=True)
+    return proj
+
+
+def test_inplace_volumes(drogon_project):
+    """Test class RmsInplaceVolumes
+
+    Args:
+        drogon_project (roxar.Project): an instance of the drogon project
+    """
+    inplace = RmsInplaceVolumes(drogon_project, "Geogrid", "geogrid_volumes")
+    assert isinstance(inplace.params, dict), (
+        "Params should be dictionary, but is" f" {type(inplace.params)}"
+    )
