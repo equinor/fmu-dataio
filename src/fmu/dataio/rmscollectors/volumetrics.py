1c111aaa000afad9bdb7ba6c4804fd2eb88b8036
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-11-28 13:15:18 +0100

    Remove RmsGrid from volumetrics

--- src/fmu/dataio/rmscollectors/volumetrics.py
+++ src/fmu/dataio/rmscollectors/volumetrics.py
@@ -267,20 +267,3 @@ class RmsInplaceVolumes:
             config_path,
             **kwargs,
         )
-
-
-@dataclass
-class RmsGrid:
-    """Class for exporting data Grid"""
-
-    project: str
-    grid_name: str
-    job_name: str
-    params: dict = None
-
-    def __post_init__(self):
-        """Initialize what is not initialized upfront"""
-        self.params = utils.get_job_arguments(
-            ["Grid models", self.grid_name, "Grid"], "Create Grid", self.job_name
-        )
-        self.project = utils._get_project(self.project, True)
