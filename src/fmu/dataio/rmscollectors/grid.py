43dfc2889ebf3286db2002b6c60a2d253721df38
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-11-29 15:02:27 +0100

    Add job to RmsGridJob

--- src/fmu/dataio/rmscollectors/grid.py
+++ src/fmu/dataio/rmscollectors/grid.py
@@ -166,15 +166,15 @@ class RmsGridJob:
     project: str
     grid_name: str
     job_name: str
+    job_type = "Create Grid"
     params: dict = None
 
     def __post_init__(self):
         """Initialize what is not initialized upfront"""
+        self.owner = ["Grid models", self.grid_name, "Grid"]
         self.project = utils._get_project(self.project, True)
-
-        self.params = utils.get_job_arguments(
-            ["Grid models", self.grid_name, "Grid"], "Create Grid", self.job_name
-        )
+        self.job = utils.get_job(self.owner, self.job_type, self.job_name)
+        self.params = utils.get_job_arguments(self.owner, self.job_type, self.job_name)
         self.dimensions = _get_grid_dimensions(self.params)
         self.general_settings = _get_general_settings(self.params)
         self.based_on = _get_horizon_model(self.params)
