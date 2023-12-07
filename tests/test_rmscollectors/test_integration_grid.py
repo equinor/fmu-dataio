0d499c5d45c8b050b6b421ccdb476de6c9a4d38e
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-12-07 15:13:03 +0100

    Expand export of grid and parameters

--- tests/test_rmscollectors/test_integration_grid.py
+++ tests/test_rmscollectors/test_integration_grid.py
@@ -65,8 +65,32 @@ def test_rmsgridjob_execute(SimgridJob):
     SimgridJob.execute()
 
 
-def test_rmsgridjob_export(SimgridJob, DROGON_FMU_CONFIG, tmp_path):
-    runpath = tmp_path / "realization-0/iter-0"
-    runpath.mkdir(parents=True, exist_ok=True)
-    os.chmod(runpath)
-    SimgridJob.export(config_path=DROGON_FMU_CONFIG)
+def test_rmsgridjob_export(SimgridJob, DROGON_FMU_CONFIG, fmurun_w_casemetadata):
+    print(f"cd into {str(fmurun_w_casemetadata)}")
+    rms_folder = fmurun_w_casemetadata / "rms/model"
+    rms_folder.mkdir(parents=True, exist_ok=True)
+    os.chdir(rms_folder)
+    exported = SimgridJob.export(config_path=DROGON_FMU_CONFIG)
+    exported_count = len(exported)
+    print(f"Exported to {exported}")
+    share_path = fmurun_w_casemetadata / "share/results"
+    print(share_path)
+    folders = list(share_path.glob("*/"))
+    assert (
+        len(folders) == 1
+    ), f"More than 1 folders ({folders}) under share created ({len(folders)})"
+    folder_name = folders[0].name
+    assert (
+        folder_name == "grids"
+    ), f"Exported to wrong folder, should be grids, but is {folder_name}"
+    metadata_objects = share_path.glob("grids/*.yml")
+    meta_count = 0
+    for metadata_object in metadata_objects:
+        obj_path = metadata_object.parent / metadata_object.name[1:].replace(".yml", "")
+        assert (
+            obj_path.exists()
+        ), f"metadata object {metadata_object.name} had no corresponding object"
+        meta_count += 1
+    assert (
+        meta_count == exported_count
+    ), f"Expected count of object to be {exported_count}, but is {meta_count}"
