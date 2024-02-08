02764c2b2483b1634256ef9e3ccc385d50843679
Jan C. Rivenaes <jan@rivenaes.net>
2024-02-08 18:24:08 +0100

    CLN: adjust examples accoring to redesign of FmuProvider
    The examples now fake ERT environment variables

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -2,8 +2,6 @@
 set -e
 # The examples scripts are ran and included in the documentation!
 
-export RUN_DATAIO_EXAMPLES=1  # this is important when running examples!
-
 current=$PWD
 
 # empty current results
@@ -13,6 +11,14 @@ rm -rf examples/s/d/nn/xcase/iter-0/*
 
 # Note! run from RUNPATH, NOT being inside RMS but need RUN_DATAIO_EXAMPLES env!
 cd $current/examples/s/d/nn/xcase/realization-0/iter-0/rms/bin
+
+# fake an ERT FMU run
+export _ERT_EXPERIMENT_ID=6a8e1e0f-9315-46bb-9648-8de87151f4c7
+export _ERT_ENSEMBLE_ID=b027f225-c45d-477d-8f33-73695217ba14
+export _ERT_SIMULATION_MODE=test_run
+export _ERT_ITERATION_NUMBER=0
+export _ERT_REALIZATION_NUMBER=0
+export _ERT_RUNPATH=$current/examples/s/d/nn/xcase/realization-0/iter-0
 
 python export_faultpolygons.py
 python export_propmaps.py
@@ -25,6 +31,10 @@ python export_volumetables.py
 # Emulate FMU run with 3 realizations and export data to disk
 for num in 0 1 9; do
     cd $current/examples/s/d/nn/xcase/realization-${num}/iter-0/rms/bin
+
+    export _ERT_REALIZATION_NUMBER=$num
+    export _ERT_RUNPATH=${current}/examples/s/d/nn/xcase/realization-${num}/iter-0
+
     python export_a_surface.py
 done
 
