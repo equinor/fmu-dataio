4f58fd783b177b271d159436c34a32057548dcff
fmu-dataio-github-action <fmu-dataio-github-action>
2024-02-16 14:38:07 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g4e8abf9',
+    VERSION: '0.1.dev1+gffc778d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
