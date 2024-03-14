27f62d3019f0921ec55cb70029f1059339584438
janbjorge <jlov@equinor.com>
2024-03-14 10:35:57 +0100

    BUG: komodo used same alias as fmu-dataio in System Information model (#519)

--- src/fmu/dataio/datastructure/meta/meta.py
+++ src/fmu/dataio/datastructure/meta/meta.py
@@ -306,7 +306,6 @@ class SystemInformation(BaseModel):
         examples=["1.2.3"],
     )
     komodo: Optional[VersionInformation] = Field(
-        alias="fmu-dataio",
         default=None,
         examples=["2023.12.05-py38"],
     )
