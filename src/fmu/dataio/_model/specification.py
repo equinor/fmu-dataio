c90db57f782f460b26507e1f36940f8bbe01b3c0
mferrera <matt.ferrera@gmail.com>
2024-09-23 08:39:50 +0200

    Make fields optional

--- src/fmu/dataio/_model/specification.py
+++ src/fmu/dataio/_model/specification.py
@@ -65,10 +65,10 @@ class TableSpecification(BaseModel):
     columns: List[str]
     """List of columns present in a table."""
 
-    num_columns: int = Field(examples=[1, 9999])
+    num_columns: Optional[int] = Field(examples=[1, 9999])
     """The number of columns in a table."""
 
-    num_rows: int = Field(examples=[1, 9999])
+    num_rows: Optional[int] = Field(examples=[1, 9999])
     """The number of rows in a table.."""
 
     size: int = Field(examples=[1, 9999])
