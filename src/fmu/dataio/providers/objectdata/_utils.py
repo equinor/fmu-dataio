5c67e34556372720915b007602d8456a052011b3
Erich Suter <esut@equinor.com>
2024-11-27 17:25:55 +0100

    ENH: Faultroom export - add mapping of juxt.pos. to SMDA names (#724)

---
+++ src/fmu/dataio/providers/objectdata/_utils.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import warnings
+
+from fmu.dataio._model.global_configuration import Stratigraphy
+
+
+class Utils:
+    @staticmethod
+    def get_stratigraphic_name(stratigraphy: Stratigraphy, name: str) -> str:
+        """
+        Get the name of a stratigraphic element from the stratigraphy.
+        name: name of stratigraphic element
+        """
+        if name in stratigraphy:
+            return stratigraphy[name].name
+
+        warnings.warn(
+            f"Stratigraphic element '{name}' not found in the stratigraphic column "
+            "in global config"
+        )
+        return ""
