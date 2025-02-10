76d6279903f23fd8c2dd5d316c8699db1090c330
Sindre Langeveld <31990241+slangeveld@users.noreply.github.com>
2025-02-10 16:06:46 +0100

    BLD: Unpin pydantic v2.10 (#980)

--- src/fmu/dataio/_models/fmu_results/global_configuration.py
+++ src/fmu/dataio/_models/fmu_results/global_configuration.py
@@ -85,7 +85,11 @@ class StratigraphyElement(BaseModel):
 
     name: str
     stratigraphic: bool = Field(default=False)
-    alias: Optional[List[str]] = Field(default_factory=list)
+
+    # TODO: Remove type ignore when Pydantic issue
+    #  https://github.com/pydantic/pydantic/issues/10950 has been solved
+    alias: Optional[List[str]] = Field(default_factory=list)  # type: ignore[arg-type]
+
     stratigraphic_alias: Optional[List[str]] = Field(default=None)
     offset: float = Field(default=0.0, allow_inf_nan=False)
     top: Optional[data.Layer] = Field(default=None)
