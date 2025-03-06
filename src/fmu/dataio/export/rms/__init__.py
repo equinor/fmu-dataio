f4c83a1fcc8538327da68909c4270a8f0720302d
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2025-03-06 22:14:30 +0100

    ENH: Add simplified export for depth prediction surfaces (#1033)

--- src/fmu/dataio/export/rms/__init__.py
+++ src/fmu/dataio/export/rms/__init__.py
@@ -1,3 +1,8 @@
 from .inplace_volumes import export_inplace_volumes, export_rms_volumetrics
+from .structure_depth_surfaces import export_structure_depth_surfaces
 
-__all__ = ["export_inplace_volumes", "export_rms_volumetrics"]
+__all__ = [
+    "export_structure_depth_surfaces",
+    "export_inplace_volumes",
+    "export_rms_volumetrics",
+]
