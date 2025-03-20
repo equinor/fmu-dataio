6fa15febdd305a10f7c47b957c002c1325d260d0
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2025-03-20 08:30:49 +0100

    SCHEMA: Add new standard result structure_depth_fault_lines (#1083)

--- src/fmu/dataio/_models/standard_results/__init__.py
+++ src/fmu/dataio/_models/standard_results/__init__.py
@@ -1,6 +1,12 @@
 from .inplace_volumes import InplaceVolumesResult, InplaceVolumesSchema
+from .structure_depth_fault_lines import (
+    StructureDepthFaultLinesResult,
+    StructureDepthFaultLinesSchema,
+)
 
 __all__ = [
     "InplaceVolumesResult",
     "InplaceVolumesSchema",
+    "StructureDepthFaultLinesSchema",
+    "StructureDepthFaultLinesResult",
 ]
