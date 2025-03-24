5130e4f6e76b93048fcac66f899dc1780842db08
mferrera <matt.ferrera@gmail.com>
2025-03-24 07:58:12 +0100

    FIX: Adjust Ert integration and test

--- src/fmu/dataio/hook_implementations/jobs.py
+++ src/fmu/dataio/hook_implementations/jobs.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from ert import plugin
-from ert.plugins.plugin_manager import hook_implementation
+import ert
 
 
-@hook_implementation
-@plugin(name="fmu_dataio")
+@ert.plugin(name="fmu_dataio")
 def installable_workflow_jobs() -> dict:
     return {}
