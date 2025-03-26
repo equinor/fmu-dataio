9b50f4e5022174f7ab8a43ad2bf08c24d8ab2677
Sindre Langeveld <slang@equinor.com>
2025-03-26 10:09:04 +0100

    ENH: Add framework for simple loaders

---
+++ tests/test_loaders/test_sumo_interface.py
@@ -0,0 +1,13 @@
+def test_get_case_from_sumo():
+    # Write test
+    assert True
+
+
+def test_get_volume_table():
+    # Write test
+    assert True
+
+
+def test_get_depth_surface():
+    # Write test
+    assert True
