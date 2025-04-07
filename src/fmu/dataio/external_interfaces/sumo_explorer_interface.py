6fc38570444b834006ad7d5cfd36d5d0d2f96c0c
Sindre Langeveld <slang@equinor.com>
2025-04-07 14:33:54 +0200

    ENH: Add generic class to handle lazy load

--- src/fmu/dataio/external_interfaces/sumo_explorer_interface.py
+++ src/fmu/dataio/external_interfaces/sumo_explorer_interface.py
@@ -5,32 +5,25 @@ from fmu.sumo.explorer.objects import Surface, Table
 
 
 class SumoExplorerInterface:
-    def __init__(self, case_id: UUID) -> None:
-        sumo = Explorer()
-        self._case = sumo.get_case_by_uuid(case_id)
+    def __init__(self, case_id: UUID, ensemble_name: str) -> None:
+        self._case_id = case_id
+        self._ensemble_name = ensemble_name
 
-    def get_inplace_volumes_standard_results(self) -> list[Table]:
+    def get_volume_tables(self) -> list[Table]:
         # Later we can probably filter on "standard_result" here
-        tables = self._case.tables.filter(content="volumes", dataformat="parquet")
+        sumo = Explorer()
+        case = sumo.get_case_by_uuid(self._case_id)
 
-        inplace_volumes_standard_results: list[Table] = []
-        for table in tables:
-            # While we wait for Sumo to add a filter for standard results
-            if "product" in table.metadata["data"]:
-                inplace_volumes_standard_results.append(table)
-
-        return inplace_volumes_standard_results
+        return case.tables.filter(
+            content="volumes", dataformat="parquet", iteration=self._ensemble_name
+        )
 
     def get_structure_depth_surfaces_standard_results(self) -> list[Surface]:
         # Later we can probably filter on "standard_result" here
-        surfaces = self._case.surfaces.filter(
-            content="depth",
+        sumo = Explorer()
+        case = sumo.get_case_by_uuid(self._case_id)
+
+        # Later we can probably filter on "standard_result" here
+        return case.surfaces.filter(
+            content="depth", dataformat="parquet", iteration=self._ensemble_name
         )
-
-        structure_depth_surfaces_standard_results: list[Surface] = []
-        for surface in surfaces:
-            # While we wait for Sumo to add a filter for standard results
-            if "product" in surface.metadata["data"]:
-                structure_depth_surfaces_standard_results.append(surface)
-
-        return structure_depth_surfaces_standard_results
--- src/fmu/dataio/load/load_standard_results.py
+++ src/fmu/dataio/load/load_standard_results.py
@@ -1,3 +1,4 @@
+from typing import Generic, TypeVar
 from uuid import UUID
 
 import numpy as np
@@ -7,55 +8,113 @@ from fmu.dataio.external_interfaces.schema_validation_interface import (
     SchemaValidationInterface,
 )
 from fmu.dataio.external_interfaces.sumo_explorer_interface import SumoExplorerInterface
-from fmu.sumo.explorer.objects import Surface, Table
+from fmu.sumo.explorer.objects import SearchContext, Table
+
+T = TypeVar("T")
+
+
+class LoadedStandardResults(Generic[T]):
+    """The generic class for loaded standard results in fmu-dataio."""
+
+    def list_realizations(self) -> list[int]:
+        return self._filter_result.realizationids  # type:ignore
+
+    def concatenate_realizations(self) -> T:
+        raise NotImplementedError
+
+    def get_realization(self, realization_id: int) -> T:
+        # What should we return here?
+        return self._filter_result.tables.filter(realization_id)  # type:ignore
+
+    def validate_metadata(self) -> None:
+        """Validate the standard results metadata against its schema"""
+
+        validator_interface = SchemaValidationInterface()
+        for metadata in self.metadata:  # type:ignore
+            # Should we return a report or fail all?
+            # Failing all for now
+            validator_interface.validate_against_schema(
+                schema_url=metadata["$schema"], data=metadata
+            )
+
+    def validate_objects(self) -> None:
+        """Validate the standard results objects against its schema"""
+
+        validator_interface = SchemaValidationInterface()
+        for object in self.standard_results:  # type:ignore
+            # Should we return a report or fail all?
+            # Failing all for now
+            data_frame = (
+                object.to_pandas().replace(np.nan, None).to_dict(orient="records")
+            )
+            validator_interface.validate_against_schema(
+                schema_url=object.metadata["data"]["product"]["file_schema"]["url"],
+                data=data_frame,
+            )
+
+
+class InplaceVolumes(LoadedStandardResults[T]):
+    """Class representing a set of Inplace Volumes in fmu-dataio."""
+
+    _inplace_volumes: list[Table] = []
+    _inplace_volumes_metadata: list[dict] = []
+
+    def __init__(self, filter_result: SearchContext) -> None:
+        self._filter_result: SearchContext = filter_result
+        self._set_inplace_volumes()
+        self._set_metadata()
+
+    @property
+    def metadata(self) -> list[dict]:
+        """Return inplace volumes metadata
+
+        Returns:
+            list[dict]: the metadata for each inplace volume object
+        """
+        return self._inplace_volumes_metadata
+
+    @property
+    def standard_results(self) -> list[Table]:
+        """Return the inplace volumes
+
+        Returns:
+            list[Table]: the metadata for each inplace volume object
+        """
+        return self._inplace_volumes
+
+    def _set_inplace_volumes(self) -> None:
+        for table in self._filter_result:
+            # While we wait for Sumo to add a filter for standard results
+            if "product" in table.metadata["data"]:
+                self._inplace_volumes.append(table)
+
+    def _set_metadata(self) -> None:
+        self._inplace_volumes_metadata = [
+            table.metadata for table in self._inplace_volumes
+        ]
+
+
+class StructureDepthSurfaces(LoadedStandardResults[T]):
+    def __init__(self) -> None:
+        raise NotImplementedError
 
 
 @experimental
-def load_structure_depth_surfaces(sumo_case_id: UUID) -> list[dict]:
+def load_inplace_volumes(case_id: UUID, ensemble_name: str) -> InplaceVolumes:
+    """Simplified interface to load inplace volumes standard results from Sumo."""
+
+    filter_result = SumoExplorerInterface(
+        case_id=case_id, ensemble_name=ensemble_name
+    ).get_volume_tables()
+
+    return InplaceVolumes(filter_result)
+
+
+@experimental
+def load_structure_depth_surfaces(
+    case_id: UUID, ensemble_name: str
+) -> StructureDepthSurfaces:
     """Simplified interface to load structure depth surfaces
     standard results from Sumo."""
 
-    structure_depth_surfaces_standard_results: list[Surface] = SumoExplorerInterface(
-        case_id=sumo_case_id,
-    ).get_structure_depth_surfaces_standard_results()
-
-    validator_service = SchemaValidationInterface()
-    for structure_depth_surface in structure_depth_surfaces_standard_results:
-        # Should we return the metadata which validates OK or fail all?
-        # Failing all for now
-        metadata = structure_depth_surface.metadata
-        validator_service.validate_against_schema(
-            schema_url=metadata["$schema"], data=metadata
-        )
-
-    return structure_depth_surfaces_standard_results
-
-
-@experimental
-def load_inplace_volumes(sumo_case_id: UUID) -> list[dict]:
-    """Simplified interface to load inplace volumes standard results from Sumo."""
-
-    inplace_volumes_standard_results: list[Table] = SumoExplorerInterface(
-        case_id=sumo_case_id,
-    ).get_inplace_volumes_standard_results()
-
-    validator_interface = SchemaValidationInterface()
-    for inplace_volume in inplace_volumes_standard_results:
-        # Should we return the metadata which validates OK or fail all?
-        # Failing all for now
-
-        metadata = inplace_volume.metadata
-        validator_interface.validate_against_schema(
-            schema_url=metadata["$schema"], data=metadata
-        )
-
-        table_data_frame = (
-            inplace_volume.to_pandas().replace(np.nan, None).to_dict(orient="records")
-        )
-
-        validator_interface.validate_against_schema(
-            schema_url=metadata["data"]["product"]["file_schema"]["url"],
-            data=table_data_frame,
-        )
-
-    return inplace_volumes_standard_results
+    raise NotImplementedError
