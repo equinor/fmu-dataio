afa358503ef611af7c52e50426d532ed267de52e
Sindre Langeveld <slang@equinor.com>
2025-04-25 12:05:41 +0200

    TST: Add tests for loader methods

--- tests/test_loaders/test_standard_result_loaders.py
+++ tests/test_loaders/test_standard_result_loaders.py
@@ -1,47 +1,75 @@
-from copy import deepcopy
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
 
 import fmu.dataio.load.load_standard_results as load_standard_results
 
 
-def test_load_inplace_volumes(metadata_examples):
-    test_case = "005140af-cdc5-448c-9f34-84bcbb3f504d"
-    ensemble_name = "iter-0"
+def test_inplace_volumes_list_realizations():
+    realization_ids = [0, 1, 2, 3]
+    with (
+        patch(
+            "fmu.dataio.external_interfaces.sumo_explorer_interface.SumoExplorerInterface.__init__",
+            return_value=None,
+        ) as class_init_mock,
+        patch(
+            "fmu.dataio.external_interfaces.sumo_explorer_interface.SumoExplorerInterface.get_realization_ids",
+            return_value=realization_ids,
+        ) as get_realizations_mock,
+    ):
+        inplace_volumes = load_standard_results.load_inplace_volumes(
+            "test_id", "some_ensemble_name"
+        )
+        assert class_init_mock.assert_called_once
 
-    product = {
-        "name": "inplace_volumes",
-        "file_schema": {
-            "version": "0.1.0",
-            "url": "https://main-fmu-schemas-prod.radix.equinor.com/schemas/file_formats/0.1.0/inplace_volumes.json",
-        },
-    }
+        actual_ids = inplace_volumes.list_realizations()
+        assert get_realizations_mock.assert_called_once
 
-    volumes_table_metadata = deepcopy(metadata_examples["table_inplace_volumes.yml"])
-    volumes_table_metadata["data"]["product"] = product
+        assert actual_ids == realization_ids
 
-    table_mock = MagicMock()
-    table_mock.metadata = volumes_table_metadata
-    volume_tables = [table_mock]
+
+def test_inplace_volumes_get_realization():
+    columns = ["FLUID", "ZONE", "REGION", "GIIP"]
+    data_frame = pd.DataFrame(columns=columns)
 
     with (
         patch(
             "fmu.dataio.external_interfaces.sumo_explorer_interface.SumoExplorerInterface.__init__",
             return_value=None,
-        ),
+        ) as class_init_mock,
         patch(
-            "fmu.dataio.external_interfaces.sumo_explorer_interface.SumoExplorerInterface.get_volume_tables",
-            return_value=volume_tables,
-        ),
+            "fmu.dataio.external_interfaces.sumo_explorer_interface.SumoExplorerInterface.get_realization",
+            return_value=[data_frame],
+        ) as get_realizations_mock,
     ):
         inplace_volumes = load_standard_results.load_inplace_volumes(
-            test_case, ensemble_name
+            "test_id", "some_ensemble_name"
         )
+        assert class_init_mock.assert_called_once
 
-        assert len(inplace_volumes._inplace_volumes) == 1
-        assert len(inplace_volumes._inplace_volumes_metadata) == 1
-        assert inplace_volumes._inplace_volumes_metadata[0] == table_mock.metadata
+        data_frames = inplace_volumes.get_realization(0)
+        assert get_realizations_mock.assert_called_once
+
+        actual_data_frame = data_frames[0]
+
+        pd.testing.assert_frame_equal(actual_data_frame, data_frame)
+
+        assert actual_data_frame.FLUID.name
+        assert actual_data_frame.ZONE.name
+        assert actual_data_frame.REGION.name
+        assert actual_data_frame.GIIP.name
+        with pytest.raises(
+            AttributeError, match="'DataFrame' object has no attribute 'STOIIP'"
+        ):
+            assert actual_data_frame.STOIIP
 
 
-def test_load_structure_depth_surfaces():
-    # Add tests
+def test_inplace_volumes_save_realization(tmp_path):
+    # TODO
+    assert True
+
+
+def test_inplace_volumes_get_blob():
+    # TODO
     assert True
