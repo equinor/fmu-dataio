d916d25cd3254ea29e3e971d02280336a68a512f
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2025-05-07 20:45:09 +0200

    SCHEMA: Set data.time formats to 'iso-date-time' instead of 'datetime'

--- src/fmu/dataio/_models/fmu_results/data.py
+++ src/fmu/dataio/_models/fmu_results/data.py
@@ -32,7 +32,13 @@ class Timestamp(BaseModel):
     )
     """A string label corresponding to the timestamp."""
 
-    value: NaiveDatetime | AwareDatetime = Field(examples=["2020-10-28T14:28:02"])
+    # pydantic sets the format of the below field to "datetime".
+    # But "datetime" requires timezones and will fail in ajv validation.
+    # Workaround is to use the "iso-date-time" format instead.
+    value: NaiveDatetime | AwareDatetime = Field(
+        examples=["2020-10-28T14:28:02"],
+        json_schema_extra={"format": "iso-date-time"},
+    )
     """A datetime representation."""
 
 
