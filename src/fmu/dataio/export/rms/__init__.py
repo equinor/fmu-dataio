8d8b5f77c61e696b0036887484bbb24e89915e2e
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2025-05-14 15:41:45 +0200

    ENH: Add simple_export for 'fluid_contact_outline'

--- src/fmu/dataio/export/rms/__init__.py
+++ src/fmu/dataio/export/rms/__init__.py
@@ -1,4 +1,5 @@
 from .field_outline import export_field_outline
+from .fluid_contact_outlines import export_fluid_contact_outlines
 from .fluid_contact_surfaces import export_fluid_contact_surfaces
 from .inplace_volumes import export_inplace_volumes, export_rms_volumetrics
 from .structure_depth_fault_lines import export_structure_depth_fault_lines
@@ -13,4 +14,5 @@ __all__ = [
     "export_rms_volumetrics",
     "export_field_outline",
     "export_fluid_contact_surfaces",
+    "export_fluid_contact_outlines",
 ]
