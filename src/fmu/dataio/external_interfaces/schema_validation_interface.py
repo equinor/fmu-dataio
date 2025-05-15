75428dd9bca2b3108a47872b4de3e84ca0872dac
Sindre Langeveld <slang@equinor.com>
2025-05-15 13:36:07 +0200

    ENH: Add data class layer to simple loader classes

--- src/fmu/dataio/external_interfaces/schema_validation_interface.py
+++ src/fmu/dataio/external_interfaces/schema_validation_interface.py
@@ -7,7 +7,13 @@ import requests
 class SchemaValidationInterface:
     @staticmethod
     @lru_cache
-    def _get_cached_validator(schema_url: str) -> jsonschema.Draft202012Validator:
+    def _get_validator(schema_url: str) -> jsonschema.Draft202012Validator:
+        """
+        Get the schema validator to use to validate data objects.
+        Use cached validator when present for the requested schema, or
+        create a new validator and add it to the cache.
+        """
+
         response: requests.Response
 
         try:
@@ -23,10 +29,11 @@ class SchemaValidationInterface:
     def validate_against_schema(
         self, schema_url: str, data: object
     ) -> bool | Exception:
-        json_schema_validator = self._get_cached_validator(schema_url)
+        """Validate the provided data object against the provided schema."""
+
+        json_schema_validator = self._get_validator(schema_url)
 
         if not json_schema_validator.is_valid(instance=data):
-            # Run the full validation to raise the validation error
             jsonschema.validate(
                 schema=json_schema_validator.schema,
                 instance=data,
