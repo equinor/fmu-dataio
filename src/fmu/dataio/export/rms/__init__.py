df3361b7b53d6387a6056dac36c0757badf73965
Erich Suter <esut@equinor.com>
2025-09-11 13:18:59 +0200

    ENH: implement structure depth fault surface standard export

--- src/fmu/dataio/export/rms/__init__.py
+++ src/fmu/dataio/export/rms/__init__.py
@@ -3,12 +3,14 @@ from .fluid_contact_outlines import export_fluid_contact_outlines
 from .fluid_contact_surfaces import export_fluid_contact_surfaces
 from .inplace_volumes import export_inplace_volumes, export_rms_volumetrics
 from .structure_depth_fault_lines import export_structure_depth_fault_lines
+from .structure_depth_fault_surfaces import export_structure_depth_fault_surfaces
 from .structure_depth_isochores import export_structure_depth_isochores
 from .structure_depth_surfaces import export_structure_depth_surfaces
 from .structure_time_surfaces import export_structure_time_surfaces
 
 __all__ = [
     "export_structure_depth_fault_lines",
+    "export_structure_depth_fault_surfaces",
     "export_structure_depth_surfaces",
     "export_structure_time_surfaces",
     "export_structure_depth_isochores",
