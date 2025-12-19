f8a9c9da189eda58add297c115c331fd97e7e8b9
nourinmohd <nourin_mohd97@outlook.com>
2025-12-19 10:44:36 +0100

    TYP: Type annotate test_simpleexport_class

--- tests/test_export_rms/test_simpleexport_class.py
+++ tests/test_export_rms/test_simpleexport_class.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 import pytest
 from fmu.datamodels.fmu_results.global_configuration import GlobalConfiguration
 
 from fmu.dataio.export._base import SimpleExportBase
 
 
-def test_validate_global_config_invalid(globalconfig1):
+def test_validate_global_config_invalid(globalconfig1: dict[str, Any]) -> None:
     invalid_config = globalconfig1.copy()
     invalid_config.pop("masterdata")
 
@@ -12,6 +14,6 @@ def test_validate_global_config_invalid(globalconfig1):
         SimpleExportBase._validate_global_config(invalid_config)
 
 
-def test_validate_global_config(globalconfig1):
+def test_validate_global_config(globalconfig1: dict[str, Any]) -> None:
     config = SimpleExportBase._validate_global_config(globalconfig1)
     assert isinstance(config, GlobalConfiguration)
