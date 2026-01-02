982bfee42c77b98af62ac7ad86511ff47069b47f
nourinmohd <nourin_mohd97@outlook.com>
2026-01-02 09:23:43 +0100

    TYP: Type annotate tests/test_units/test_run_context.py

--- tests/test_units/test_run_context.py
+++ tests/test_units/test_run_context.py
@@ -2,43 +2,44 @@ from pathlib import Path
 
 import pytest
 from fmu.datamodels.fmu_results.enums import FMUContext
+from pytest import MonkeyPatch
 
 from fmu.dataio._definitions import RMSExecutionMode
 from fmu.dataio._runcontext import RunContext, get_rms_exec_mode
 
 
 @pytest.mark.usefixtures("inside_rms_interactive")
-def test_inside_rms_decorator():
+def test_inside_rms_decorator() -> None:
     assert get_rms_exec_mode() is not None
     assert get_rms_exec_mode() == RMSExecutionMode.interactive
 
 
-def test_get_rms_exec_mode_batch(monkeypatch):
+def test_get_rms_exec_mode_batch(monkeypatch: MonkeyPatch) -> None:
     """Test the rms execution mode in RMS batch."""
     monkeypatch.setenv("RUNRMS_EXEC_MODE", "batch")
     assert get_rms_exec_mode() == RMSExecutionMode.batch
 
 
-def test_get_rms_exec_mode_interactive(monkeypatch):
+def test_get_rms_exec_mode_interactive(monkeypatch: MonkeyPatch) -> None:
     """Test the rms execution mode in RMS interactive."""
     monkeypatch.setenv("RUNRMS_EXEC_MODE", "interactive")
     assert get_rms_exec_mode() == RMSExecutionMode.interactive
 
 
-def test_get_rms_exec_mode_outside(monkeypatch):
+def test_get_rms_exec_mode_outside(monkeypatch: MonkeyPatch) -> None:
     """Test that rms execution mode outside of RMS is None."""
     monkeypatch.delenv("RUNRMS_EXEC_MODE", raising=False)
     assert get_rms_exec_mode() is None
 
 
-def test_get_rms_exec_mode_unknown(monkeypatch):
+def test_get_rms_exec_mode_unknown(monkeypatch: MonkeyPatch) -> None:
     """Test that an unknow rms execution mode fails."""
     monkeypatch.setenv("RUNRMS_EXEC_MODE", "unknown")
     with pytest.raises(ValueError, match="not a valid"):
         get_rms_exec_mode()
 
 
-def test_runcontext_rms_interactive(monkeypatch):
+def test_runcontext_rms_interactive(monkeypatch: MonkeyPatch) -> None:
     """Test the RunContext in RMS interactive."""
     monkeypatch.setenv("RUNRMS_EXEC_MODE", "interactive")
 
@@ -56,7 +57,9 @@ def test_runcontext_rms_interactive(monkeypatch):
     assert runcontext.exportroot == Path.cwd().parent.parent
 
 
-def test_runcontext_rms_batch_inside_fmu(monkeypatch, fmurun_w_casemetadata):
+def test_runcontext_rms_batch_inside_fmu(
+    monkeypatch: MonkeyPatch, fmurun_w_casemetadata: Path
+) -> None:
     """Test the RunContext inside FMU realization context with RMS in batch."""
     monkeypatch.setenv("RUNRMS_EXEC_MODE", "batch")
 
@@ -68,13 +71,16 @@ def test_runcontext_rms_batch_inside_fmu(monkeypatch, fmurun_w_casemetadata):
 
     assert runcontext.runpath == fmurun_w_casemetadata
     assert runcontext.casepath == fmurun_w_casemetadata.parent.parent
+    assert runcontext.case_metadata is not None
     assert runcontext.case_metadata.fmu.case.name == "somecasename"
 
     # exportroot should be the runpath
     assert runcontext.exportroot == runcontext.runpath
 
 
-def test_runcontext_outside_rms_inside_fmu(monkeypatch, fmurun_w_casemetadata):
+def test_runcontext_outside_rms_inside_fmu(
+    monkeypatch: MonkeyPatch, fmurun_w_casemetadata: Path
+) -> None:
     """Test the RunContext inside FMU in a realization context outside of RMS."""
     monkeypatch.delenv("RUNRMS_EXEC_MODE", raising=False)
 
@@ -86,13 +92,16 @@ def test_runcontext_outside_rms_inside_fmu(monkeypatch, fmurun_w_casemetadata):
 
     assert runcontext.runpath == fmurun_w_casemetadata
     assert runcontext.casepath == fmurun_w_casemetadata.parent.parent
+    assert runcontext.case_metadata is not None
     assert runcontext.case_metadata.fmu.case.name == "somecasename"
 
     # exportroot should be the runpath
     assert runcontext.exportroot == runcontext.runpath
 
 
-def test_runcontext_inside_fmu_prehook(monkeypatch, fmurun_prehook):
+def test_runcontext_inside_fmu_prehook(
+    monkeypatch: MonkeyPatch, fmurun_prehook: Path
+) -> None:
     """Test the RunContext inside FMU in a case context, outside of RMS."""
     monkeypatch.delenv("RUNRMS_EXEC_MODE", raising=False)
 
@@ -106,13 +115,16 @@ def test_runcontext_inside_fmu_prehook(monkeypatch, fmurun_prehook):
 
     assert runcontext.runpath is None
     assert runcontext.casepath == fmurun_prehook
+    assert runcontext.case_metadata is not None
     assert runcontext.case_metadata.fmu.case.name == "somecasename"
 
     # exportroot should be the casepath
     assert runcontext.exportroot == runcontext.casepath
 
 
-def test_runcontext_inside_fmu_prehook_no_casepath(monkeypatch, fmurun_prehook):
+def test_runcontext_inside_fmu_prehook_no_casepath(
+    monkeypatch: MonkeyPatch, fmurun_prehook: Path
+) -> None:
     """
     Test the RunContext inside FMU in a case context, outside of RMS.
     Test without casepath provided which will give warning here, but will raise an
@@ -136,7 +148,9 @@ def test_runcontext_inside_fmu_prehook_no_casepath(monkeypatch, fmurun_prehook):
     assert runcontext.exportroot == Path.cwd()
 
 
-def test_runcontext_inside_fmu_prehook_invalid_casepath(monkeypatch, fmurun_prehook):
+def test_runcontext_inside_fmu_prehook_invalid_casepath(
+    monkeypatch: MonkeyPatch, fmurun_prehook: Path
+) -> None:
     """
     Test the RunContext inside FMU in a case context, outside of RMS.
     Test with an invalid casepath provided which will give warning here, but will
@@ -160,7 +174,7 @@ def test_runcontext_inside_fmu_prehook_invalid_casepath(monkeypatch, fmurun_preh
     assert runcontext.exportroot == Path.cwd()
 
 
-def test_runcontext_outside(monkeypatch):
+def test_runcontext_outside(monkeypatch: MonkeyPatch) -> None:
     """Test the RunContext outside RMS and outside FMU."""
     monkeypatch.delenv("RUNRMS_EXEC_MODE", raising=False)
 
