d8270ab319a982c75609701be257a04b123dcb09
mferrera <13275515+mferrera@users.noreply.github.com>
2026-03-09 08:24:16 +0100

    ENH: Constrain select parameter field types

--- src/fmu/dataio/_workflows/case/_parameters.py
+++ src/fmu/dataio/_workflows/case/_parameters.py
@@ -6,6 +6,7 @@ import logging
 from typing import TYPE_CHECKING, Final
 
 import ert
+import pyarrow as pa
 from pydantic import TypeAdapter
 
 from fmu.datamodels import ErtParameterMetadata
@@ -14,7 +15,6 @@ from ._config import CaseWorkflowConfig
 
 if TYPE_CHECKING:
     import polars as pl
-    import pyarrow as pa
 
 
 logger: Final = logging.getLogger(__name__)
@@ -23,6 +23,33 @@ logger.setLevel(logging.CRITICAL)
 ErtParameterMetadataAdapter: TypeAdapter[ErtParameterMetadata] = TypeAdapter(
     ErtParameterMetadata
 )
+
+
+def _genkw_to_metadata(config: ert.config.GenKwConfig) -> ErtParameterMetadata:
+    """Convert GenKwConfig to parameter metadata."""
+    distribution_dict = config.distribution.model_dump(exclude={"name"})
+    return ErtParameterMetadataAdapter.validate_python(
+        {
+            "group": config.group or "DEFAULT",
+            "input_source": config.input_source,
+            "distribution": config.distribution.name.lower(),
+            **distribution_dict,
+        }
+    )
+
+
+def _resolve_pa_field_type(name: str, pa_type: pa.DataType) -> pa.DataType:
+    """Ensures some PyArrow field types are changed to smaller ones.
+
+    In particular, APS produces some columns that have simple text fields with one
+    word. These come by default as large_string()."""
+    if name == "REAL" and pa.types.is_int64(pa_type):
+        return pa.int32()
+    if pa.types.is_large_unicode(pa_type):
+        return pa.utf8()
+    if pa.types.is_large_string(pa_type):
+        return pa.string()
+    return pa_type
 
 
 def _process_parameters(
@@ -53,31 +80,22 @@ def _process_parameters(
             rename_map[col_name] = param_name
         else:
             columns_to_drop.append(col_name)
-            logger.warning(f"Skipping '{col_name}': no valid GenKwConfig found")
+            logger.info(f"Skipping '{col_name}': no valid GenKwConfig found")
 
     scalars_df = scalars_df.drop(columns_to_drop).rename(rename_map)
     arrow_table = scalars_df.to_arrow()
 
     fields = [
-        pa.field(f.name, f.type, metadata=metadata_map.get(f.name))
+        pa.field(
+            f.name,
+            _resolve_pa_field_type(f.name, f.type),
+            metadata=metadata_map.get(f.name),
+        )
         for f in arrow_table.schema
     ]
     table = arrow_table.cast(pa.schema(fields))
 
     return table, realizations
-
-
-def _genkw_to_metadata(config: ert.config.GenKwConfig) -> ErtParameterMetadata:
-    """Convert GenKwConfig to parameter metadata."""
-    distribution_dict = config.distribution.model_dump(exclude={"name"})
-    return ErtParameterMetadataAdapter.validate_python(
-        {
-            "group": config.group or "DEFAULT",
-            "input_source": config.input_source,
-            "distribution": config.distribution.name.lower(),
-            **distribution_dict,
-        }
-    )
 
 
 def get_ert_parameters_table(
